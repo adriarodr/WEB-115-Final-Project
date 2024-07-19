f73896e32e635f080aba694a90c2a32fbcec4be9
adriarodr <158720918+adriarodr@users.noreply.github.com>
2024-07-18 23:05:03 -0400

    JQuery

--- projectJS.js
+++ projectJS.js
@@ -102,9 +102,16 @@ $(document).ready(function () {
         // Create a new window
         const newWin = window.open("", "Week Meal Plan");
 
+        // Write onto the new window
         newWin.document.write(`
             <html>
-                <head></head>
+                <head>
+                    <link rel="stylesheet" href="css/styles.css">
+
+                    <link rel="preconnect" href="https://fonts.googleapis.com">
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+                </head>
                 <body>
                     <h1>Meal Plan for the week</h1>
                     <p>Welcome ${name}</p>
@@ -172,12 +179,11 @@ $(document).ready(function () {
                             <th>${weekMealPlan.sunDinner}</th>
                         </tr>
                     </table>
+                    <button onclick="print()" class="newWinBtn">Print my meal plan</button>
                 </body>
             </html>
         `);
     }
-
-    // Print Planner
 
     // Download Planner
 
